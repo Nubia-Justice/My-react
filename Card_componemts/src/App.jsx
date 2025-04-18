720805e95b60f425f0abed2c8a2fd05454372f99
Nubia Justice <jice6135@gmail.com>
2025-04-18 18:09:40 +0100

    First commit

---
+++ Card_componemts/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card.jsx";
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+export default App;
+// Compare this snippet from my-react-app/src/App.jsx:
+// import Header from "./Header";
+// import Footer from "./Footer.jsx";
